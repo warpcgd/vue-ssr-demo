73f9fd05cd09e9b726b8a6ea6e160f62e8c051f7
Phillips <warpcgd@qq.com>
2019-08-12 18:13:24 +0800

    feat: stage-2-vue项目

---
+++ App.vue
@@ -0,0 +1,25 @@
+// app.vue
+<template>
+  <div>
+    hello world
+    <div v-if='show'>show me</div>
+    <div>
+      <button @click='toggle'>toggle</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      show: true
+    }
+  },
+  methods: {
+    toggle() {
+      this.show = !this.show
+    }
+  }
+}
+</script>
